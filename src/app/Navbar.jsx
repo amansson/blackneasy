9f4939c079d562d3d96953b574b28baee7e13f0f
Andreas Månsson <amanssons@gmail.com>
2020-07-04 08:59:10 +0200

    navbar - add markup and styles - add fontawesome package - add colors to start on color variables - add temp home page

---
+++ src/app/Navbar.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+    return (
+        <div className="nav">
+            <label className="logo">Black and Easy</label>
+            <nav className="links">
+                <NavLink exact to="/" className="link" activeClassName="link active">Home</NavLink>
+                <NavLink exact to="/about" className="link" activeClassName="link active">About</NavLink>
+                <NavLink exact to="/services" className="link" activeClassName="link active">Services</NavLink>
+                <NavLink exact to="/contact" className="link" activeClassName="link active">Contact</NavLink>
+                <NavLink exact to="/feedback" className="link" activeClassName="link active">Feedback</NavLink>
+            </nav>
+            <a href="#" className="cta"><button>Contact</button></a>
+        </div>
+    )
+}
+
+export default Navbar;
