93da2547f595303b82538e7c16c4168a66444e40
Andreas Månsson <amanssons@gmail.com>
2020-07-04 08:59:10 +0200

    navbar - add markup and styles - add fontawesome package - add colors to start on color variables - add temp home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Navbar from './app/Navbar';
+import Landing from './pages/Home';
 
 const App = () => {
-
-  return (
-    <div className="App">
-      <h1>
-          React App
-      </h1>
-    </div>
-  );
-
+    return (
+        <Fragment>
+            <Navbar />
+            <Switch>
+                <Route path="/" component={Landing} exact />
+            </Switch>
+        </Fragment>
+    )
 }
 
 export default App;
