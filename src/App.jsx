005c813caf1bd7406387ac0d87cedea44efcdbdf
Andreas Månsson <amanssons@gmail.com>
2020-07-04 17:31:22 +0200

    move footer to correct place

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Navbar from './app/Navbar';
+import Footer from './app/Footer';
 import Landing from './pages/Home';
-import Footer from './pages/Footer';
 
 const App = () => {
     return (
