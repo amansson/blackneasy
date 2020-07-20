ab55698a48959a7fe1b65b8b4ca04a82a67c3f08
Andreas Månsson <amanssons@gmail.com>
2020-07-20 10:31:15 +0200

    Modal - add markup - add style - works as separte modal example now

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -1,8 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import FetchData from '../components/FetchData';
 import { Image, Transformation, CloudinaryContext } from 'cloudinary-react';
+import { FaWindowClose } from 'react-icons/fa';
 
 const Certificate = () => {
+
+
+    const [modalState, setModalState] = useState(false);
+
+    const toggleModal = () => {
+        setModalState(!modalState);
+    };
 
     const imageData = FetchData('https://res.cloudinary.com/blackneasy/image/list/mh.json', {});
     if (!imageData.response) {
@@ -12,12 +20,30 @@ const Certificate = () => {
     return (
         <Fragment>
             <h1>MH</h1>
+            <div className={modalState ? "modalBackground modalShow" : "modalBackground"}>
+                <div className="modalInner">
+                    <button onClick={toggleModal} className="exitButton"><FaWindowClose /></button>
+                    <img src="https://images.unsplash.com/photo-1439066615861-d1af74d74000?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1866&q=80" alt="" />
+                    <h2>This our Image Caption</h2>
+                </div>
+            </div>
+            <button onClick={toggleModal}>Open modal</button>
+            <br />
+            <br />
             <div className="cards-certificate">
                 {
                     imageData.response.resources.map(image => {
                         return (
                             <div className="card" key={image.public_id}>
-                                <Image cloudName="blackneasy" width="400" height="300" crop="thumb" alt={image.public_id + "_mh"} publicId={image.public_id} />
+                                <Image cloudName="blackneasy" publicId={image.public_id}>
+                                    <Transformation
+                                        crop="scale"
+                                        width="300"
+                                        height="200"
+                                        dpr="auto"
+                                        responsive_placeholder="blank"
+                                    />
+                                </Image>
                                 <div className="caption">{image.context.custom.caption}</div>
                             </div>
                         )
