715865af51a49e2763db0dfacae8e323689153ef
Andreas Månsson <amanssons@gmail.com>
2020-07-20 14:54:15 +0200

    Puppies - add puppies page marku and style - add puppy component markup and style - add navbar links to puppies

---
+++ src/pages/Puppies.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Puppy from '../components/Puppy';
+import FetchData from '../components/FetchData';
+
+const Puppies = () => {
+
+    const imageData = FetchData('https://res.cloudinary.com/blackneasy/image/list/puppy.json', {});
+    if (!imageData.response) {
+        return <div className="loading">Loading...</div>
+    }
+
+    return (
+        <div>
+            <div className="cards-puppies">
+                {
+                    imageData.response.resources.map(image => {
+                        return (
+                            <Puppy key={image.public_id} image_id={image.public_id} />
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Puppies;
