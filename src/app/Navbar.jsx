f2855820777a556fcecf6849dc49e343ebde326c
Andreas Månsson <amanssons@gmail.com>
2020-07-21 15:33:27 +0200

    Gallery - add markup and style - using new masonry component for dispplaying photos - temp photos - add routing and link

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
                 <NavLink exact to="/certificate" className="link" activeClassName="link active">MH och HD</NavLink>
                 <NavLink exact to="/puppies" className="link" activeClassName="link active">Valpar</NavLink>
                 <NavLink exact to="/brood" className="link" activeClassName="link active">Kullar</NavLink>
-                <NavLink exact to="/photo" className="link" activeClassName="link active">Fotocollage</NavLink>
+                <NavLink exact to="/gallery" className="link" activeClassName="link active">Fotocollage</NavLink>
             </nav>
             <button onClick={toggleSidebar} className="nav-button"><FaAlignRight /></button>
         </div>
