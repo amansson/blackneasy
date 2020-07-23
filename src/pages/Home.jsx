6d171753a54e63abefe1b4e7e42d6afa6af7d302
Andreas Månsson <amanssons@gmail.com>
2020-07-23 15:20:53 +0200

    add banner with image background and text information

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import Cloudinary from '../components/Cloudinary';
+import React, { Fragment } from 'react';
+import BreedJixie from '../components/BreedJixie';
 
 const Landing = () => {
     return (
         <Fragment>
-    
+            <div className="banner">
+                <div className="container">
+                    <h1>Black n easy <strong>Valpar i alla dess former</strong></h1>
+                    <p>Tervuerens, Groendahl</p>                    
+                </div>
+            </div>
+
             <div className="wrapper">
                 <h1>Parning</h1>
                 <BreedJixie />
-        </div>
+            </div>
         </Fragment>
     )
 }
