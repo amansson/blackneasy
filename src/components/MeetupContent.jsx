1c42bfc129cc3129de16ff5be385525d8e89606d
Andreas Månsson <amanssons@gmail.com>
2020-07-31 16:58:18 +0200

    fix modal in meetup, required to have its own component. Can't iterate over a modal in the same component

--- src/components/MeetupContent.jsx
+++ src/components/MeetupContent.jsx
@@ -1,15 +1,8 @@
 import React, { useState, Fragment } from 'react';
-import { Image, Transformation, CloudinaryContext } from 'cloudinary-react';
-import Modal from '../components/Modal';
 import FetchData from './FetchData';
+import MeetupContentExtend from './MeetupContentExtend';
 
 const MeetupContent = (props) => {
-    const [showModal, setShowModal] = useState(false);
-
-    const toggleModal = () => {
-        setShowModal(!showModal);
-    }
-
     const imageData = FetchData(`https://res.cloudinary.com/blackneasy/image/list/${props.meetup}.json`, {});
     if (!imageData.response) {
         return <div className="loading">Loading...</div>
@@ -19,25 +12,7 @@ const MeetupContent = (props) => {
         <Fragment>
             {imageData.response.resources.map(image => {
                 return (
-                    <Fragment>
-                        <div className="card" onClick={toggleModal}>
-                            <Image cloudName="blackneasy" publicId={image.public_id}>
-                                <Transformation
-                                    crop="fill"
-                                    gravity="faces"
-                                    width="300"
-                                    height="200"
-                                    dpr="auto"
-                                    responsive_placeholder="blank"
-                                />
-                            </Image>
-                        </div>
-                        {
-                            showModal ?
-                                <Modal showModal={toggleModal} image={<Image cloudName="blackneasy" publicId={image.public_id}></Image>} />
-                                : ""
-                        }
-                    </Fragment>
+                    <MeetupContentExtend key={image.public_id} image={image.public_id} />
                 )
             })}
         </Fragment>
