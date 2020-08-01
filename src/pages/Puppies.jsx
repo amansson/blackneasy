97be1221a645453e560bb75528f92d5a4f12ab1b
Andreas Månsson <amanssons@gmail.com>
2020-08-01 10:37:35 +0200

    change style of mother and father selection

--- src/pages/Puppies.jsx
+++ src/pages/Puppies.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Puppy from '../components/Puppy';
 import FetchData from '../components/FetchData';
+import { FaHeart } from 'react-icons/fa';
 
 const Puppies = () => {
 
@@ -10,15 +11,72 @@ const Puppies = () => {
     }
 
     return (
-        <div className="wrapper cards-puppies">
-            {
-                imageData.response.resources.map(image => {
-                    return (
-                        <Puppy key={image.public_id} image_id={image.public_id} />
-                    )
-                })
-            }
-        </div>
+        <Fragment>
+            <div className="wrapper">
+                <h2 className="notice-action">Klicka på Mamma - Pappa för att se deras valparna</h2>
+                <div className="card-puppies">
+                    <div className="puppies-select">
+                        <div class="select">
+                            <img src="https://res.cloudinary.com/blackneasy/image/upload/v1596117516/Easy.jpg" />
+                            <div class="select-infos">
+                                <h2>Easy</h2>
+                                <p>Mamma</p>
+                            </div>
+                        </div>
+                        <div className="bg">
+                            <div class="avatar-bordered">
+                                <div className="heart"><FaHeart /></div>
+                            </div>
+                        </div>
+                        <div class="select">
+                            <img src="https://res.cloudinary.com/blackneasy/image/upload/v1593848797/Jixie.jpg" alt="" />
+                            <div class="select-infos">
+                                <h2>Jixie</h2>
+                                <p>Pappa</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="puppies-select">
+                        <div class="select">
+                            <img src="https://res.cloudinary.com/blackneasy/image/upload/v1596117516/Easy.jpg" />
+                            <div class="select-infos">
+                                <h2>Easy</h2>
+                                <p>Mamma</p>
+                            </div>
+                        </div>
+                        <div className="bg">
+                            <div class="avatar-bordered">
+                                <div className="heart"><FaHeart /></div>
+                            </div>
+                        </div>
+                        <div class="select">
+                            <img src="https://res.cloudinary.com/blackneasy/image/upload/v1593848797/Jixie.jpg" alt="" />
+                            <div class="select-infos">
+                                <h2>Jixie</h2>
+                                <p>Pappa</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="wrapper puppies-images">
+                {
+                    imageData.response.resources.map(image => {
+                        return (
+                            <Puppy key={image.public_id} image_id={image.public_id} />
+                        )
+                    })
+                }
+            </div>
+
+        </Fragment>
     )
 }
 
