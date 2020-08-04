1a11f6c9cb2fe89c42099304a2aedc828230b8a3
Andreas Månsson <amanssons@gmail.com>
2020-08-04 15:55:56 +0200

    - update animal style and markup - change name to match other component

--- src/pages/Animal.jsx
+++ src/pages/Animal.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import AnimalCard from '../components/AnimalCard';
-
+import AnimalContent from '../components/AnimalContent';
+import FetchData from '../components/FetchData';
 
 const Animal = () => {
+    const imageData = FetchData('https://res.cloudinary.com/blackneasy/image/list/animal.json', {});
+    if (!imageData.response) {
+        return <div className="loading">Loading...</div>
+    }
+
+    console.log(imageData.response.resources);
+
     return (
-        <div className="wrapper cards">
-            <AnimalCard name={"Jixie"} />        
-            <AnimalCard name={"Sample"} />        
-            <AnimalCard name={"Jixie"} />        
-            <AnimalCard name={"Jixie"} />        
-            <AnimalCard name={"Jixie"} />        
-            <AnimalCard name={"Jixie"} />        
+        <div className="wrapper cards-animal">
+            {
+                imageData.response.resources.map(image => {
+                    return (
+                        <AnimalContent key={image.public_id} image={image.public_id} name={image.context.custom.caption} breed={image.context.custom.breed} other={image.context.custom.other} />
+                    )
+                })
+            }
         </div>
     )
 }
