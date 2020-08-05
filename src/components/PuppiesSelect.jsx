6bba33d956468f95098aa243c5441cebc54edb99
Andreas Månsson <amanssons@gmail.com>
2020-08-05 09:44:45 +0200

    reworked the stuyle of the mother father component to take less space and for image content

--- src/components/PuppiesSelect.jsx
+++ src/components/PuppiesSelect.jsx
@@ -1,45 +1,29 @@
 import React from 'react';
 import { Image, Transformation } from 'cloudinary-react';
-import { FaHeart } from 'react-icons/fa';
 
 const PuppiesSelect = (props) => {
     return (
         <div className="puppies-select" onClick={props.toggle}>
-            <div class="select">
+            
                 <Image cloudName="blackneasy" publicId={props.imageMother} >
-                    <Transformation
-                            crop="fill"
-                            gravity="faces"
-                            width="300"
-                            height="200"
-                            dpr="auto"
-                            responsive_placeholder="blank"
-                        />
-                        </Image>
-
-                
-                <div class="select-infos">
-                    <h2>{props.nameMother}</h2>
-                    <p>Mamma</p>
-                </div>
-            </div>
-            <div className="bg">
-                <div class="avatar-bordered">
-                    <div className="heart"><FaHeart /></div>
-                </div>
-            </div>
-            <div class="select">
-                <Image cloudName="blackneasy" publicId={props.imageFather}>
-                    <Transformation
-                        crop="scale"
-                        gravity="faces"
-                    />
+                    <Transformation crop="fill" gravity="faces" width="300" height="200" />
                 </Image>
-                <div class="select-infos">
-                    <h2>{props.nameFather}</h2>
-                    <p>Pappa</p>
+
+                <div className="infos">
+                    <div className="item-left">
+                        <h2>{props.nameMother}</h2>
+                        <p>Mamma</p>
+                    </div>
+                    <div className="item-right">
+                        <h2>{props.nameFather}</h2>
+                        <p>Pappa</p>
+                    </div>
                 </div>
-            </div>
+
+                <Image cloudName="blackneasy" publicId={props.imageFather}>
+                    <Transformation crop="fill" gravity="faces" width="300" height="200" />
+                </Image>
+            
         </div>
     )
 }
