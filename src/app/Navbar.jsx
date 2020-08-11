d348872eb13a4cc8644a5f056e9f71819504e2c9
Andreas Månsson <amanssons@gmail.com>
2020-08-11 14:45:13 +0200

    bug sidebar opened in normal window mode, now fixed

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -13,14 +13,14 @@ const Navbar = () => {
         <div className="nav">
             <NavLink exact to="/" className="logo">Black and Easy</NavLink>
             <nav className={sidebarOpen ? "side" : "top"}>
-                <NavLink onClick={toggleSidebar} exact to="/" className="link" activeClassName="link active">Hem</NavLink>
-                <NavLink onClick={toggleSidebar} exact to="/animal" className="link" activeClassName="link active">Djur</NavLink>
-                <NavLink onClick={toggleSidebar} exact to="/certificate" className="link" activeClassName="link active">MH och HD</NavLink>
-                <NavLink onClick={toggleSidebar} exact to="/puppies" className="link" activeClassName="link active">Valpar</NavLink>
-                <NavLink onClick={toggleSidebar} exact to="/brood" className="link" activeClassName="link active">Kullar</NavLink>
-                <NavLink onClick={toggleSidebar} exact to="/about" className="link" activeClassName="link active">Om Mig</NavLink>
-                <NavLink onClick={toggleSidebar} exact to="/gallery" className="link" activeClassName="link active">Fotocollage</NavLink>
-                <NavLink onClick={toggleSidebar} exact to="/meetup" className="link" activeClassName="link active">Kennel Träff</NavLink>
+                <NavLink onClick={sidebarOpen ? toggleSidebar : ""} exact to="/" className="link" activeClassName="link active">Hem</NavLink>
+                <NavLink onClick={sidebarOpen ? toggleSidebar : ""} exact to="/animal" className="link" activeClassName="link active">Djur</NavLink>
+                <NavLink onClick={sidebarOpen ? toggleSidebar : ""} exact to="/certificate" className="link" activeClassName="link active">MH och HD</NavLink>
+                <NavLink onClick={sidebarOpen ? toggleSidebar : ""} exact to="/puppies" className="link" activeClassName="link active">Valpar</NavLink>
+                <NavLink onClick={sidebarOpen ? toggleSidebar : ""} exact to="/brood" className="link" activeClassName="link active">Kullar</NavLink>
+                <NavLink onClick={sidebarOpen ? toggleSidebar : ""} exact to="/about" className="link" activeClassName="link active">Om Mig</NavLink>
+                <NavLink onClick={sidebarOpen ? toggleSidebar : ""} exact to="/gallery" className="link" activeClassName="link active">Fotocollage</NavLink>
+                <NavLink onClick={sidebarOpen ? toggleSidebar : ""} exact to="/meetup" className="link" activeClassName="link active">Kennel Träff</NavLink>
             </nav>
             <button onClick={toggleSidebar} className="nav-button"><FaAlignRight /></button>
         </div>
