c0a9da5c23ddb244c22b6738deca0616b8b2d080
Andreas Månsson <amanssons@gmail.com>
2020-08-11 13:07:49 +0200

    change puppie select card style

--- src/components/PuppiesSelect.jsx
+++ src/components/PuppiesSelect.jsx
@@ -4,26 +4,24 @@ import { Image, Transformation } from 'cloudinary-react';
 const PuppiesSelect = (props) => {
     return (
         <div className="puppies-select" onClick={props.toggle}>
-            
-                <Image cloudName="blackneasy" publicId={props.imageMother} >
-                    <Transformation crop="fill" gravity="faces" width="300" height="200" />
-                </Image>
 
-                <div className="infos">
-                    <div className="item-left">
-                        <h2>{props.nameMother}</h2>
-                        <p>Mamma</p>
-                    </div>
-                    <div className="item-right">
-                        <h2>{props.nameFather}</h2>
-                        <p>Pappa</p>
-                    </div>
-                </div>
+            <Image cloudName="blackneasy" publicId={props.imageMother} >
+                <Transformation crop="fill" gravity="faces" width="300" height="200" />
+            </Image>
 
-                <Image cloudName="blackneasy" publicId={props.imageFather}>
-                    <Transformation crop="fill" gravity="faces" width="300" height="200" />
-                </Image>
-            
+            <Image cloudName="blackneasy" publicId={props.imageFather}>
+                <Transformation crop="fill" gravity="faces" width="300" height="200" />
+            </Image>
+
+            <div className="infos">
+                <h2>{props.nameMother}</h2>
+                <p>Mamma</p>
+            </div>
+
+            <div className="infos">
+                <h2>{props.nameFather}</h2>
+                <p>Pappa</p>
+            </div>
         </div>
     )
 }
