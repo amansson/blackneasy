e584a6f30b80c26a1fdee06549c75576c878fe0f
Andreas Månsson <amanssons@gmail.com>
2020-08-14 13:26:20 +0200

    Updates

---
+++ precache-manifest.4650b08483d6279d3bba3b862cf5b10e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d7cb20d043f520e18e793984314e9c",
+    "url": "/blackneasy/index.html"
+  },
+  {
+    "revision": "0b009d7d4f7750f55b1c",
+    "url": "/blackneasy/static/css/main.c6ced01e.chunk.css"
+  },
+  {
+    "revision": "df8b2329a3e7299bd843",
+    "url": "/blackneasy/static/js/2.86a7b672.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/blackneasy/static/js/2.86a7b672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b009d7d4f7750f55b1c",
+    "url": "/blackneasy/static/js/main.d682beb0.chunk.js"
+  },
+  {
+    "revision": "ebc83dc44bcafc5c46fa",
+    "url": "/blackneasy/static/js/runtime-main.e1e0cb0b.js"
+  }
+]);
