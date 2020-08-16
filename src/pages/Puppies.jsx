763288b6234277302eeb21e366ee17a6ef7516d1
Andreas Månsson <amanssons@gmail.com>
2020-08-16 14:12:49 +0200

    add shantis and kazan

--- src/pages/Puppies.jsx
+++ src/pages/Puppies.jsx
@@ -9,6 +9,7 @@ const Puppies = () => {
     const [showMother4, setShowMother4] = useState(false);
     const [showMother5, setShowMother5] = useState(false);
     const [showMother6, setShowMother6] = useState(false);
+    const [showMother7, setShowMother7] = useState(false);
 
     const toggleMother1 = () => {
         toggleAll();
@@ -37,6 +38,10 @@ const Puppies = () => {
         toggleAll();
         setShowMother6(!showMother6);
     }
+    const toggleMother7 = () => {
+        toggleAll();
+        setShowMother7(!showMother7);
+    }
 
     const toggleAll = () => {
         setShowMother1(false);
@@ -45,12 +50,13 @@ const Puppies = () => {
         setShowMother4(false);
         setShowMother5(false);
         setShowMother6(false);
+        setShowMother7(false);
     }
 
     return (
         <Fragment>
             <div className="wrapper">
-                <h2 className="notice-action">Klicka på Mamma - Pappa för att se deras valparna</h2>
+                <h2 className="notice-action">Klicka på Mamma - Pappa för att se deras valparna och scrolla</h2>
                 <div className="card-puppies">
                     <PuppiesSelect toggle={toggleMother1} imageMother={"profile/Easy_profile"} imageFather={"profile/Kastor_profile"} nameMother={"Easy"} nameFather={"Kastor"} />
                     <PuppiesSelect toggle={toggleMother2} imageMother={"profile/Vimsa_profile"} imageFather={"profile/Zkrutt_profile"} nameMother={"Vimsa"} nameFather={"Zkrutt"} />
@@ -58,6 +64,7 @@ const Puppies = () => {
                     <PuppiesSelect toggle={toggleMother4} imageMother={"profile/Vimsa_profile"} imageFather={"profile/Kazan_profile"} nameMother={"Vimsa"} nameFather={"Kazan"} />
                     <PuppiesSelect toggle={toggleMother5} imageMother={"profile/Tiffy_profile"} imageFather={"profile/"} nameMother={"Tiffy"} nameFather={"Hane"} />
                     <PuppiesSelect toggle={toggleMother6} imageMother={"profile/Jixie_profile"} imageFather={"profile/Proffen_profile"} nameMother={"Jixie"} nameFather={"Proffen"} />
+                    <PuppiesSelect toggle={toggleMother7} imageMother={"profile/Shantis_profile"} imageFather={"profile/Kazan_profile"} nameMother={"Shantis"} nameFather={"Kazan"} />
                 </div>
 
                 {showMother1 ?
@@ -105,6 +112,14 @@ const Puppies = () => {
                         <h2 className="text-align-center">Jixie och Proffen</h2>
                         <div className="wrapper puppies-images">
                             <PuppiesContent puppies={"puppies_jixie_proffen"} />
+                        </div>
+                    </div>
+                    : ""}
+                {showMother7 ?
+                    <div className="select-divider">
+                        <h2 className="text-align-center">Shantis och Kazan</h2>
+                        <div className="wrapper puppies-images">
+                            <PuppiesContent puppies={"puppies_shantis_kazan"} />
                         </div>
                     </div>
                     : ""}
