c87b54632a0878122beee215c21f0ff700d473d2
Andreas Månsson <amanssons@gmail.com>
2020-09-25 17:05:03 +0200

    add cindy breeding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ const Landing = () => {
             </div>
 
             <div className="wrapper">
+                <Breed header={"Parade"} images={""} imageMother={"profile/cinderella_profile"} imageFather={"profile/Leo_profile"} nameMother={"Cindy"} nameFather={"Leo"} />
                 <Breed header={"Parades 21 Juni 2020"} images={"breed_leda_proffen"} imageMother={"profile/leda_profile"} imageFather={"profile/proffen_profile"} nameMother={"Leda"} nameFather={"Proffen"} />
                 <Breed header={"Parades 21 Juni 2020"} images={"breed_jixie_proffen"} imageMother={"profile/jixie_profile"} imageFather={"profile/proffen_profile"} nameMother={"Jixie"} nameFather={"Proffen"} />
             </div>
