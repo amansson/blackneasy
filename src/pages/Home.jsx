ea57ea31d595567a7f8896daaad1b138d5692b70
Andreas Månsson <amanssons@gmail.com>
2020-12-12 08:52:29 +0100

    add christmas image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Breed from '../components/Breed';
+import { Image, Transformation } from 'cloudinary-react';
 
 const Landing = () => {
     return (
@@ -9,6 +10,10 @@ const Landing = () => {
                     <p>Belgisk Vallhund / Tervuerens, Groendahl, Russkaya Tsvetnaya Bolonka</p>
                 </div>
             </div>
+
+            <Image cloudName="blackneasy" publicId="christmas" className="center">
+                <Transformation height="500" crop="fill" />
+            </Image>
 
             <div className="wrapper">
                 <Breed header={"Parade - Russkaya Tsvetnasa Bolonka - Väntar valpar vecka 40 2020"} images={""} imageMother={"breed/Cinderella_breed"} imageFather={"breed/FeyerVerk_breed2"} nameMother={"Black'n Easy - Cinderella"} nameFather={"Russkaya Roskosh - Feyer Verk"} />
