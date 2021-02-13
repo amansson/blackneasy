26e5909d8ca6dfb6a4756f32b61aff56e85214f9
Andreas Månsson <amanssons@gmail.com>
2021-02-13 10:48:28 +0100

    remove cindy and leda breed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,7 @@ const Landing = () => {
             </div>
 
             <div className="wrapper">
-                <Breed header={"Parade - Russkaya Tsvetnasa Bolonka - Väntar valpar vecka 40 2020"} images={""} imageMother={"breed/Cinderella_breed"} imageFather={"breed/FeyerVerk_breed2"} nameMother={"Black'n Easy - Cinderella"} nameFather={"Russkaya Roskosh - Feyer Verk"} />
-                <Breed header={"Parades 21 Juni 2020"} images={"breed_leda_proffen"} imageMother={"profile/leda_profile"} imageFather={"profile/proffen_profile"} nameMother={"Leda"} nameFather={"Proffen"} />
+                
             </div>
         </Fragment>
     )
