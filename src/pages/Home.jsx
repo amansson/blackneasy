87dd00a7c1a98bab4bc6d99cf007713b306c6ab7
Andreas Månsson <amanssons@gmail.com>
2021-03-23 17:08:59 +0100

    add new breed leda, jixie and dream boy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Breed from '../components/Breed';
 import BreedPlan from '../components/BreedPlan';
+import { FaHeart } from 'react-icons/fa';
 
 const Landing = () => {
     return (
@@ -12,7 +13,7 @@ const Landing = () => {
             </div>
 
             <div className="wrapper">
-                <BreedPlan header={"Groenendael och Tervuerens"} header2={"Planerad parning, valpar Vår/Sommar"} images={"puppies_shantis_zkrutt"} />
+                <BreedPlan header={"Valpar väntas mellan Sp1 LD Startklass RLDN Thunderwolfs Dream Boy ❤ och Black'n Easys Leda ❤ och  Black'n Easys Jixie ❤"} header2={"Groenendael o Tervuerens Leveransklara till midsommar"} images={"breed_leda_jixie_dreamboy"} />
             </div>
         </Fragment>
     )
