6aac8a76aab8d4217888ad87f38128cf2e478c62
Andreas Månsson <amanssons@gmail.com>
2021-08-07 12:36:45 +0200

    add cindy puppies and remove leda and jixie puppies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,13 +8,12 @@ const Landing = () => {
         <Fragment>
             <div className="banner">
                 <div>
-                    <p>Leda och Jixie har fött valpar se nedan för mer information, Kontakt: Marie-Louise, 0706-170833, stadning@hotmail.com</p>
+                    <p>Cindy har fött valpar se nedan för mer information, Kontakt: Marie-Louise, 0706-170833, stadning@hotmail.com</p>
                 </div>
             </div>
 
             <div className="wrapper">
-                <Breed header={"Valpar födda 19 April 2021"} images={"puppies_leda_dreamboy"} imageMother={"profile/leda_profile"} imageFather={"profile/Dreamboy_profile"} nameMother={"Leda (SE44579/2017)"} nameFather={"Dream boy (SE38782/2017)"} />
-                <Breed header={"Valpar födda 26 April 2021"} images={"puppies_jixie_dreamboy"} imageMother={"profile/jixie_profile"} imageFather={"profile/Dreamboy_profile"} nameMother={"Jixie (SE55765/2016)"} nameFather={"Dream boy (SE38782/2017)"} />
+                <Breed header={"Valpar - Russkaya Tsvetnasa Bolonka - Cindy födde 4 valpar - 3 Augusti 2021"} images={"puppies_cindy_linus"} imageMother={"breed/Cinderella_breed"} imageFather={"breed/Linus"} nameMother={"Black'n Easy Cinderella - AS59730/2017"} nameFather={"Mellvins Milarious Buggati (Linus) - AS43295/2019"} />                
             </div>
         </Fragment>
     )
