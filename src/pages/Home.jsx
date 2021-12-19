ffce1767d3f6e88c2acfad2e3806c6a2dee68424
Andreas Månsson <amanssons@gmail.com>
2021-12-19 18:40:32 +0100

    remove breed and add christmas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,19 @@ import React, { Fragment } from 'react';
 import Breed from '../components/Breed';
 import BreedPlan from '../components/BreedPlan';
 import { FaHeart } from 'react-icons/fa';
+import Christmas from '../components/Christmas';
 
 const Landing = () => {
     return (
         <Fragment>
             <div className="banner">
                 <div>
-                    <p>Cindy har fött valpar se nedan för mer information, Kontakt: Marie-Louise, 0706-170833, stadning@hotmail.com</p>
+                    <p>Kontakt: Marie-Louise, 0706-170833, stadning@hotmail.com</p>
                 </div>
             </div>
 
             <div className="wrapper">
-                <Breed header={"Valpar - Russkaya Tsvetnasa Bolonka - Cindy födde 3 valpar (1 tik och 2 hanar) - 3 Augusti 2021"} images={"puppies_cindy_linus"} imageMother={"breed/Cinderella_breed"} imageFather={"breed/Linus"} nameMother={"Black'n Easy Cinderella - AS59730/2017"} nameFather={"Mellvins Milarious Buggati (Linus) - AS43295/2019"} />                
+                <Christmas />
             </div>
         </Fragment>
     )
