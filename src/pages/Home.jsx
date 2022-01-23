694f36267504d4f4a44800d1ac1c78dff97d699a
Andreas Månsson <amanssons@gmail.com>
2022-01-23 15:25:44 +0100

    add Tassa and Shiro breeding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,18 @@ const Landing = () => {
         <Fragment>
             <div className="banner">
                 <div>
-                    <p>Kontakt: Marie-Louise, 0706-170833, stadning@hotmail.com</p>
+                    <p>Tassa har fött valpar se nedan för mer information, Kontakt: Marie-Louise, 0706-170833, stadning@hotmail.com</p>
                 </div>
             </div>
 
             <div className="wrapper">
-                
+                <Breed header={"Valpar - Russkaya Tsvetnasa Bolonka - Tassa födde 2 valpar (2 tikar) - 17 December 2021"}
+                    images={"puppies_tassa_shiro"}
+                    imageMother={"breed/Tassa_breed"}
+                    imageFather={"breed/Shiro_breed"}
+                    nameMother={"Black'n Easy Tittolina (Tassa) - AS10974/2020"}
+                    nameFather={"Shiro Sho Khazar - SE15096/2021"}
+                />
             </div>
         </Fragment>
     )
