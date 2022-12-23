4ec48343a8fe739be5e84fc6766a5b4d6a79c77d
Andreas Månsson <amansson@gmail.com>
2022-12-23 10:56:54 +0100

    remove old breedings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,23 +13,7 @@ const Landing = () => {
                 </div>
             </div>
             <div className="wrapper">
-                <Breed header={"RUSSKAJA TSVETSNAJA BOLONKA - Valpar 16/8 2022 (1 tik och 3 hanar)"}
-                    header2={"Intresserad av en liten Bolonka valp? Leveransklara mitten av oktober"}
-                    images={"puppies_tassa_khazar"}
-                    imageMother={"profile/Tassa_profile"}
-                    imageFather={"profile/Khazar_profile"}
-                    nameMother={"Black'n Easys Tassa"}
-                    nameFather={"Khazar Shiro Sho"}
-                />
-            </div>
-            <div className="wrapper">
-                <Breed header={"Tysk Kleinspetitz - valpar 10 April (3 hanar och 1 tik, alla svarta) "}
-                    images={"puppies_brownie_fellboms"}
-                    imageMother={"profile/Brownie_profile"}
-                    imageFather={"profile/Fellboms_profile"}
-                    nameMother={"Ellinghort's Golden EYE (Brownie)"}
-                    nameFather={"Fellboms Czabo - SE50391/2016 C.I.B, SEUCH, DKUCH, NOUCH, Nordic Show Champion, NO V-21 "}
-                />
+                
             </div>
         </Fragment>
     )
